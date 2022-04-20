8a7537d725e0d499a61ce288dd4d757391f04947
Sergio Lopez <sergio@softnya.com>
2022-04-19 20:19:40 -0500

    Practica de my_launchx_app

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,6 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const fer = new MissionCommander("Fernanda")
+
+console.log(carlo.name)
+console.log(fer.name)
