3bd0d18c62d4425386d3b9e7bf8c458acd5727ad
Sergio Lopez <sergio@softnya.com>
2022-04-20 21:04:05 -0500

    Prueba unitaria 2 y 3 de twitter

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -9,4 +9,19 @@ describe('Tests for UserService', () => {
         expect(user.name).toBe('Carlo')
         expect(user.bio).not.toBeUndefined()
     })
+
+    test('2. Get all user data in a list', () => {
+        const user = UserService.create(1, 'carlogilmar', 'Carlo')
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe('carlogilmar')
+        expect(userInfoInList[2]).toBe('Carlo')
+        expect(userInfoInList[3]).toBe('Sin bio')
+    })
+
+    test('3. Update username', () => {
+        const user = UserService.create(1, 'carlogilmar', 'Carlo')
+        UserService.updateUsername(user, 'carlog')
+        expect(user.username).toBe('carlog')
+    })
 })
