50bfa42d39500d63e22bd853bb94ee9c2eda15b0
Sergio Lopez <sergio@softnya.com>
2022-04-20 20:56:43 -0500

    Clase UserService creada

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -1,7 +1,7 @@
 const UserService = require('./../../app/services/UserService')
 
 describe('Tests for UserService', () => {
-    TextDecoderStream('1. Create a new user unsing the UserService', () => {
+    test('1. Create a new user unsing the UserService', () => {
         const user = UserService.create(1, 'carlogilmar', 'Carlo')
 
         expect(user.id).toBe(1)
