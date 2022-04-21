ef78e0f2e8144174dfd12abab4687b9bc26b6d96
Sergio Lopez <sergio@softnya.com>
2022-04-20 21:22:55 -0500

    Prueba 3 app twitter

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -13,4 +13,10 @@ describe('Tests for UserView', () => {
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
+
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser (payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    })
 })
